3b62352b74a32dfa70fe0ad0d2864b8ec7e7247c
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2023-06-14 23:03:35 +0800

    Update tinylisp_extra_funcs.lisp

--- tinylisp_extra_funcs.lisp
+++ tinylisp_extra_funcs.lisp
@@ -1,4 +1,25 @@
 
+;;list
+(define make-list-1 (lambda (k v l) (if (= k 0) l (make-list-1 (- k 1) v (append (list v) l)))))
+(define make-list (lambda (k v) (let* (l (list)) (make-list-1 k v l))))
+(define linspace-1
+  (lambda (x d x1 l)
+	(cond
+	  ((<= x x1) (linspace-1 (+ x d) d x1 (append l (list x))))
+	  (t l))))
+(define linspace-2
+  (lambda (x d x1 l)
+	(cond
+	  ((>= x x1) (linspace-2 (+ x d) d x1 (append l (list x))))
+	  (t l))))
+(define linspace
+  (lambda (x0 d x1)
+	(cond
+	  ((and (> x1 x0) (> d 0)) (linspace-1 x0 d x1 (list)))
+	  ((and (< x1 x0) (< d 0)) (linspace-2 x0 d x1 (list)))
+	  ((= x1 x0) (list x0))
+	  (t <err>))))
+
 ;;binary
 (define nthb (lambda (k n) (ash (logand n (ash 1 k)) (* -1 k))))
 (define setb (lambda (k n) (logor n (ash 1 k))))
